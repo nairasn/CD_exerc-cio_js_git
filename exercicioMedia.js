587f6c58e5b57c834d2ddb9af5b0e684ea0a00de
Naira <nascimentosnaira@gmail.com>
2021-10-06 21:40:55 -0300

    duas pastas teste

---
+++ exercicioMedia.js
@@ -0,0 +1,82 @@
+Aluno1 = prompt("Digite o nome do aluno 1:")
+notas1 = prompt("Digite as notas no espaço, separadas por vírgula: ")
+notas1 = notas1.split(",")
+nota1 = notas1[0]
+nota2 = notas1[1]
+nota3 = notas1[2]
+media1 = (parseFloat(nota1) + parseFloat(nota2) + parseFloat(nota3))/3; 
+
+if (media1 >= 5) {
+	alert("Aprovado! Partiu férias!");
+} else {
+	alert ("Reprovado. Partiu Recu :( ");
+}
+
+Aluno2 = prompt("Digite o nome do aluno 2:")
+notas2 = prompt("Digite as notas no espaço, separadas por vírgula: ")
+notas2 = notas2.split(",")
+nota1 = notas2[0]
+nota2 = notas2[1]
+nota3 = notas2[2]
+media2 = (parseFloat(nota1) + parseFloat(nota2) + parseFloat(nota3))/3;
+ 
+if (media2 >= 5) {
+	alert("Aprovado! Partiu férias!");
+} else {
+	alert ("Reprovado. Partiu Recu :( ");
+}
+
+Aluno3 = prompt("Digite o nome do aluno 3:")
+notas3 = prompt("Digite as notas no espaço, separadas por vírgula: ")
+notas3 = notas3.split(",")
+nota1 = notas3[0]
+nota2 = notas3[1]
+nota3 = notas3[2]
+media3 = (parseFloat(nota1) + parseFloat(nota2) + parseFloat(nota3))/3; 
+
+if (media3 >= 5) {
+	alert("Aprovado! Partiu férias!");
+} else {
+	alert ("Reprovado. Partiu Recu :( ");
+}
+
+Aluno4 = prompt("Digite o nome do aluno 3:")
+notas4 = prompt("Digite as notas no espaço, separadas por vírgula: ")
+notas4 = notas4.split(",")
+nota1 = notas4[0]
+nota2 = notas4[1]
+nota3 = notas4[2]
+media4 = (parseFloat(nota1) + parseFloat(nota2) + parseFloat(nota3))/3; 
+
+if (media4 >= 5) {
+	alert("Aprovado! Partiu férias!");
+} else {
+	alert ("Reprovado. Partiu Recu :( ");
+}
+
+Aluno5 = prompt("Digite o nome do aluno 3:")
+notas5 = prompt("Digite as notas no espaço, separadas por vírgula: ")
+notas5 = notas5.split(",")
+nota1 = notas5[0]
+nota2 = notas5[1]
+nota3 = notas5[2]
+media5 = (parseFloat(nota1) + parseFloat(nota2) + parseFloat(nota3))/3; 
+
+if (media5 >= 5) {
+	alert("Aprovado! Partiu férias!");
+} else {
+	alert ("Reprovado. Partiu Recu :( ");
+}
+
+mensagem = ""
+''
+if(a == 1) mensagem += "Nota 1\n"
+'Nota 1\n'
+if(b == 2) mensagem += "Nota 2\n"
+'Nota 1\nNota 2\n'
+if(c == 3) mensagem += "Nota 3\n"
+'Nota 1\nNota 2\nNota 3\n'
+mensagem
+
+alert(mensagem)
+
